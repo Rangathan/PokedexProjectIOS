83d2d6735eddd07cc8637ea44d278ec9d439cf7d
Rangathan <akreyken@gmail.com>
2024-03-13 15:50:09 -0700

    Update PokemonListView.swift

--- PokedexProjectIOS/PokemonListView.swift
+++ PokedexProjectIOS/PokemonListView.swift
@@ -11,41 +11,74 @@ import Combine
 
 
 
-
 struct PokemonItemViewContainer: View {
     let pokemon: Pokemon
+    let dimensions: Double = 140
     @ObservedObject var viewModel: PokemonListViewModel
     
     var body: some View {
-        if let details = viewModel.pokemonDetails[pokemon.name] {
-            VStack {
-                if let spriteURL = URL(string: details.sprites.frontDefault) {
-                    AsyncImage(url: spriteURL) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(height: 100)
-                    } placeholder: {
-                        ProgressView()
+        VStack {
+            if let details = viewModel.pokemonDetails[pokemon.name] {
+                VStack {
+                    if let spriteURL = URL(string: details.sprites.frontDefault) {
+                        AsyncImage(url: spriteURL) { image in
+                            image
+                                
+                                 // Fixed size for image
+                        } placeholder: {
+                            ProgressView()
+                                .frame(width: dimensions, height: dimensions)
+                        }
                     }
+                    
+                    Text(pokemon.name)
+                        .font(.caption)
+                        .multilineTextAlignment(.center)
+                        .frame(width: dimensions) // Fixed width for text
                 }
-                
-                Text(pokemon.name)
-                    .font(.caption)
-                    .multilineTextAlignment(.center)
+                .padding(8)
+                .background(determineBackgroundColor())
+                .clipShape(Circle())
+                // Half of the width of VStack container
+                // Fixed size for VStack container
+            } else {
+                ProgressView()
+                    .onAppear {
+                        viewModel.fetchPokemonDetails(for: pokemon)
+                    }
+                    .clipShape(Circle())
             }
-            .padding(8)
-            .background(Color.blue)
-            .cornerRadius(8)
-        } else {
-            ProgressView()
-                .onAppear {
-                    viewModel.fetchPokemonDetails(for: pokemon)
-                }
         }
     }
+    private func determineBackgroundColor() -> Color {
+            guard let details = viewModel.pokemonDetails[pokemon.name],
+                  let type = details.types.first?.type.name.lowercased() else {
+                // Default color if details are not available
+                return Color.gray
+            }
+            
+            switch type {
+                case "grass":
+                    return Color.green
+                case "fire":
+                    return Color.red
+                case "water":
+                    return Color.blue
+                case "electric":
+                    return Color(red: 1, green: 1, blue: 1) // White
+                case "psychic":
+                    return Color.black
+                case "fighting":
+                    return Color(red: 1, green: 1, blue: 1) // White
+                case "flying":
+                    return Color(red: 1, green: 1, blue: 1) // White
+                case "poison":
+                    return Color.green
+                default:
+                    return Color.gray
+            }
+        }
 }
-
 
 
 extension View {
@@ -54,13 +87,6 @@ extension View {
     }
 }
 
-struct PokemonItemViewPlaceholder: View {
-    var body: some View {
-        Rectangle()
-            .foregroundColor(.gray)
-            .aspectRatio(1, contentMode: .fit)
-    }
-}
 
 
 
