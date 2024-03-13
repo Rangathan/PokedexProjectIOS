8cccd3826b4d3fa9c0d15af232c9e3489242393f
Rangathan <akreyken@gmail.com>
2024-03-13 16:52:25 -0700

    Update PokemonListView.swift

--- PokedexProjectIOS/PokemonListView.swift
+++ PokedexProjectIOS/PokemonListView.swift
@@ -51,34 +51,74 @@ struct PokemonItemViewContainer: View {
         }
     }
     private func determineBackgroundColor() -> Color {
-            guard let details = viewModel.pokemonDetails[pokemon.name],
-                  let type = details.types.first?.type.name.lowercased() else {
-                // Default color if details are not available
-                return Color.gray
-            }
-            
-            switch type {
-                case "grass":
-                    return Color.green
-                case "fire":
-                    return Color.red
-                case "water":
-                    return Color.blue
-                case "electric":
-                    return Color(red: 1, green: 1, blue: 1) // White
-                case "psychic":
-                    return Color.black
-                case "fighting":
-                    return Color(red: 1, green: 1, blue: 1) // White
-                case "flying":
-                    return Color(red: 1, green: 1, blue: 1) // White
-                case "poison":
-                    return Color.green
-                default:
-                    return Color.gray
-            }
+        guard let details = viewModel.pokemonDetails[pokemon.name],
+              let type = details.types.first?.type.name.lowercased() else {
+            // Default color if details are not available
+            return Color.gray
         }
+        
+        switch type {
+        case "normal":
+            return Color(hex: "A8A77A")
+        case "fire":
+            return Color(hex: "EE8130")
+        case "water":
+            return Color(hex: "6390F0")
+        case "electric":
+            return Color(hex: "F7D02C")
+        case "grass":
+            return Color(hex: "7AC74C")
+        case "ice":
+            return Color(hex: "96D9D6")
+        case "fighting":
+            return Color(hex: "C22E28")
+        case "poison":
+            return Color(hex: "A33EA1")
+        case "ground":
+            return Color(hex: "E2BF65")
+        case "flying":
+            return Color(hex: "A98FF3")
+        case "psychic":
+            return Color(hex: "F95587")
+        case "bug":
+            return Color(hex: "A6B91A")
+        case "rock":
+            return Color(hex: "B6A136")
+        case "ghost":
+            return Color(hex: "735797")
+        case "dragon":
+            return Color(hex: "6F35FC")
+        case "dark":
+            return Color(hex: "705746")
+        case "steel":
+            return Color(hex: "B7B7CE")
+        case "fairy":
+            return Color(hex: "D685AD")
+        case "stellar":
+            return Color(hex: "F7D02C")
+        default:
+            return Color.gray
+        }
+    }
+
 }
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+
+        if scanner.scanHexInt64(&rgbValue) {
+            let red = Double((rgbValue & 0xFF0000) >> 16) / 255.0
+            let green = Double((rgbValue & 0x00FF00) >> 8) / 255.0
+            let blue = Double(rgbValue & 0x0000FF) / 255.0
+            self.init(red: red, green: green, blue: blue)
+            return
+        }
+
+        self.init(red: 0, green: 0, blue: 0) // Return black for any invalid input
+    }
+}
+
 
 
 extension View {
